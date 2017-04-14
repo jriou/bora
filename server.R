24b7f5502534522f017e8d616dea3364bdbd4ccd
jriou <julien.riou.k@gmail.com>
2017-04-14 18:19:37 +0200

    Refined controls on data, serial interval, priors

--- server.R
+++ server.R
@@ -7,30 +7,34 @@ library(cowplot)
 function(input, output, session) {
   
   # Data management on imported file
-  ec = reactive({
+  csvdata = reactive({
     req(input$epidcurve)
     x = unlist(unname(read.csv(input$epidcurve$datapath)))
     data.frame(NCASES=x,NWEEK=1:length(x))
   })
   
+  exdata = reactive({
+    req(input$exdata)
+    if(input$exdata=="Zika virus in Martinique, 2015-2017 (W1-8)") data.frame(NCASES=c(57,119,145,456,619,1069,1629,1183),NWEEK=1:8)
+  })
+  
+  ec = reactive({
+    if(!is.null(exdata())) {
+      exdata()
+    } else {
+      csvdata()
+    }
+  })
+  
   # Plot epidemic curve
   output$plotepidcurve = renderPlot(
-    ggplot(ec()) + 
-      geom_col(aes(x=NWEEK,y=NCASES),fill="grey",colour="black",alpha=.6,width=1) + 
-      labs(title="Epidemic curve",x="Weeks",y="N") +
-      theme_bw()
+      ggplot(ec()) + 
+        geom_col(aes(x=NWEEK,y=NCASES),fill="grey",colour="black",alpha=.6,width=1) + 
+        labs(title="Epidemic curve",x="Weeks",y="N") +
+        theme_bw()
   )
   
-  # Management of serial interval distribution
-  sidistr = reactive({
-    req(input$sidisttype,input$simean,input$sisd)
-    ff = data.frame(x=seq(0,input$simean+5*input$sisd,by=0.001))
-    if(input$sidisttype=="Normal") ff$prob = dnorm(ff$x,mean=input$simean,sd=input$sisd)
-    if(input$sidisttype=="Gamma") {
-      ff$prob = dgamma(ff$x,rate=input$simean/(input$sisd^2),shape=(input$simean^2)/(input$sisd^2))
-    }
-    return(ff)
-  })
+  # Controls on serial interval distribution
   
   output$siunif <- reactive({
     req(input$sidisttype)
@@ -40,7 +44,7 @@ function(input, output, session) {
   
   output$simeansd <- reactive({
     req(input$sidisttype)
-    input$sidisttype %in% c("Normal","Log-normal","Gamma")
+    input$sidisttype %in% c("Normal","Gamma")
   })
   outputOptions(output, "simeansd", suspendWhenHidden = FALSE) 
   
@@ -56,17 +60,42 @@ function(input, output, session) {
       updateNumericInput(session,"sisd",value=0.6)
     }
   })
+  
+  # Management of serial interval distribution
+  sidistr = reactive({
+    req(input$sidisttype)
+    if(input$sidisttype=="Normal") {
+      req(input$simean,input$sisd)
+      ff = data.frame(x=seq(0,input$simean+5*input$sisd,by=0.001))
+      ff$prob = dnorm(ff$x,mean=input$simean,sd=input$sisd)
+      ff$prob = ff$prob/sum(ff$prob)
+      return(list(type="Normal",mean=input$simean,sd=input$sisd,ff=ff))
+    }
+    if(input$sidisttype=="Gamma") {
+      req(input$simean,input$sisd)
+      ff = data.frame(x=seq(0,input$simean+5*input$sisd,by=0.001))
+      ff$prob = dgamma(ff$x,rate=input$simean/(input$sisd^2),shape=(input$simean^2)/(input$sisd^2))
+      return(list(type="Gamma",mean=input$simean,sd=input$sisd,rate=input$simean/(input$sisd^2),shape=(input$simean^2)/(input$sisd^2),ff=ff))
+    }
+    if(input$sidisttype=="Uniform") {
+      req(input$simin,input$simax)
+      ff = data.frame(x=seq(0,input$simax*1.1,by=0.001))
+      ff$prob = dunif(ff$x,min=input$simin,max=input$simax)
+      return(list(type="Uniform",min=input$simin,max=input$simax,ff=ff))
+    }
+  })
 
   # Plot serial interval distribution
   output$plotsi = renderPlot(
-    ggplot(sidistr()) +
+    if(input$sidisttype!="-")
+    ggplot(sidistr()$ff) +
       geom_ribbon(aes(x=x,ymax=prob),ymin=0,fill="grey",alpha=0.6) +
       geom_line(aes(x=x,y=prob),size=.7) +
       labs(title="Distribution of the serial interval",x="Weeks",y="Density") +
       theme_bw()
   )
   
-  # Managing priors
+  # Controls on prior on r0
   output$r0unif <- reactive({
     req(input$r0disttype)
     input$r0disttype=="Uniform"
@@ -75,9 +104,124 @@ function(input, output, session) {
   
   output$r0meansd <- reactive({
     req(input$r0disttype)
-    input$r0disttype %in% c("Log-normal","Gamma")
+    input$r0disttype %in% c("Normal","Gamma")
   })
   outputOptions(output, "r0meansd", suspendWhenHidden = FALSE) 
+  
+  output$r0exp <- reactive({
+    req(input$r0disttype)
+    input$r0disttype %in% c("Exponential")
+  })
+  outputOptions(output, "r0exp", suspendWhenHidden = FALSE) 
+
+  observe({
+    if(input$exr0=="Non-informative prior distribution") {
+      updateSelectInput(session,"r0disttype",selected="Exponential")
+      updateNumericInput(session,"r0rate",value=0.5)
+    }
+    if(input$exr0=="Chikungunya virus in Martinique, 2013-2015") {
+      updateSelectInput(session,"r0disttype",selected="Gamma")
+      updateNumericInput(session,"r0mean",value=1.3)
+      updateNumericInput(session,"r0sd",value=0.1)
+    }
+  })
+  
+  
+  # Management of prior on r0
+  r0distr = reactive({
+    req(input$r0disttype)
+    if(input$r0disttype=="Uniform") {
+      req(input$r0min,input$r0max)
+      ff = data.frame(x=seq(0,input$r0max*1.1,by=0.001))
+      ff$prob = dunif(ff$x,min=input$r0min,max=input$r0max)
+      return(list(type="Uniform",min=input$r0min,max=input$r0max,ff=ff))
+    }
+    if(input$r0disttype=="Exponential") {
+      req(input$r0rate)
+      ff = data.frame(x=seq(0,2.5/input$r0rate,by=0.001))
+      ff$prob = dexp(ff$x,rate=input$r0rate)
+      return(list(type="Exponential",rate=input$r0rate,ff=ff))
+    }
+    if(input$r0disttype=="Normal") {
+      req(input$r0mean,input$r0sd)
+      ff = data.frame(x=seq(0,input$r0mean+5*input$r0sd,by=0.001))
+      ff$prob = dnorm(ff$x,mean=input$r0mean,sd=input$r0sd)
+      ff$prob = ff$prob/sum(ff$prob)
+      return(list(type="Normal",mean=input$r0mean,sd=input$r0sd,ff=ff))
+    }
+    if(input$r0disttype=="Gamma") {
+      req(input$r0mean,input$r0sd)
+      ff = data.frame(x=seq(0,input$r0mean+5*input$r0sd,by=0.001))
+      ff$prob = dgamma(ff$x,rate=input$r0mean/(input$r0sd^2),shape=(input$r0mean^2)/(input$r0sd^2))
+      return(list(type="Gamma",mean=input$r0mean,sd=input$r0sd,rate=input$r0mean/(input$r0sd^2),shape=(input$r0mean^2)/(input$r0sd^2),ff=ff))
+    }
+  })
+  
+  # Plot prior distribution on r0
+  output$plotpriorr0 = renderPlot(
+    if(input$r0disttype!="-")
+      ggplot(r0distr()$ff) +
+      geom_ribbon(aes(x=x,ymax=prob),ymin=0,fill="grey",alpha=0.6) +
+      geom_line(aes(x=x,y=prob),size=.7) +
+      labs(title="Prior distribution on the reproduction number",x=expression(R[0]),y="Density") +
+      theme_bw()
+  )
+  
+  # Controls on prior on rho
+  
+  output$rhounif <- reactive({
+    req(input$rhodisttype)
+    input$rhodisttype=="Uniform"
+  })
+  outputOptions(output, "rhounif", suspendWhenHidden = FALSE) 
+  
+  output$rhobetad <- reactive({
+    req(input$rhodisttype)
+    input$rhodisttype %in% c("Beta")
+  })
+  outputOptions(output, "rhobetad", suspendWhenHidden = FALSE) 
+  
+  observe({
+    if(input$exrho=="Non-informative prior distribution") {
+      updateSelectInput(session,"rhodisttype",selected="Beta")
+      updateNumericInput(session,"rhoshape1",value=1)
+      updateNumericInput(session,"rhoshape2",value=1)
+    }
+    if(input$exrho=="Chikungunya virus in Martinique, 2013-2015") {
+      updateSelectInput(session,"rhodisttype",selected="Beta")
+      updateNumericInput(session,"rhoshape1",value=32)
+      updateNumericInput(session,"rhoshape2",value=132)
+    }
+  })
+  
+  
+  # Management of prior on rho
+  rhodistr = reactive({
+    req(input$rhodisttype)
+    if(input$rhodisttype=="Uniform") {
+      req(input$rhomin,input$rhomax)
+      ff = data.frame(x=seq(0,1,by=0.001))
+      ff$prob = dunif(ff$x,min=input$rhomin,max=input$rhomax)
+      return(list(type="Uniform",min=input$rhomin,max=input$rhomax,ff=ff))
+    }
+    if(input$rhodisttype=="Beta") {
+      req(input$rhoshape1,input$rhoshape2)
+      ff = data.frame(x=seq(0,1,by=0.001))
+      ff$prob = dbeta(ff$x,shape1=input$rhoshape1,shape2=input$rhoshape2)
+      return(list(type="Beta",shape1=input$rhoshape1,shape2=input$rhoshape2,ff=ff))
+    }
+  })
+  
+  # Plot prior distribution on rho
+  output$plotpriorrho = renderPlot(
+    if(input$rhodisttype!="-")
+      ggplot(rhodistr()$ff) +
+      geom_ribbon(aes(x=x,ymax=prob),ymin=0,fill="grey",alpha=0.6) +
+      geom_line(aes(x=x,y=prob),size=.7) +
+      labs(title="Prior distribution on the reporting rate",x=expression(rho),y="Density") +
+      theme_bw()
+  )
+  
   
   
 }
--- ui.R
+++ ui.R
@@ -9,42 +9,57 @@ fluidPage(
   
   # 1 - Import epidemic curve
   sidebarLayout(position="left",
-                sidebarPanel(h4("Epidemic curve"),br(),
+                sidebarPanel(h4("Epidemic data"),br(),
                              fileInput("epidcurve","Select CSV file",accept=c('text/csv','text/comma-separated-values,text/plain','.csv')),
-                             selectInput("exdata","or use a provided example",choices=c("-","ZIKV Martinique 2016","ZIKV Guadeloupe 2016"))),
+                             selectInput("exdata","or use a provided example",choices=c("-","Zika virus in Martinique, 2015-2017 (W1-8)"))),
                 mainPanel(column(10,plotOutput(outputId="plotepidcurve",width="400px",heigh="200px")))
   ),
   sidebarLayout(position="left",
                 sidebarPanel(h4("Distribution of the serial interval"),br(),
-                             selectInput("sidisttype","Select a distribution",choices=c("-","Normal","Log-normal","Gamma","Uniform")),
-                             conditionalPanel(condition="output.siunif==true",
-                                              numericInput("simin","Minimum (in weeks)",min=0,max=15,value=NA,step=0.1),
-                                              numericInput("simax","Maximum (in weeks)",min=0,max=15,value=NA,step=0.1)),
+                             selectInput("sidisttype","Distribution type",choices=c("-","Uniform","Normal","Gamma")),
                              conditionalPanel(condition="output.simeansd==true",
                                               numericInput("simean","Mean (in weeks)",min=0,max=15,value=NA,step=0.1),
                                               numericInput("sisd","Standard deviation (in weeks)",min=0,max=15,value=NA,step=0.1)),
+                             conditionalPanel(condition="output.siunif==true",
+                                              numericInput("simin","Minimum (in weeks)",min=0,max=15,value=NA,step=0.1),
+                                              numericInput("simax","Maximum (in weeks)",min=0,max=15,value=NA,step=0.1)),
                              selectInput("exsi","or use a provided example",choices=c("-","Zika virus (Ae. aegypti, 28°C)","Chikungunya virus (Ae. aegypti, 28°C)"))
                 ),
                 mainPanel(column(10,plotOutput(outputId="plotsi",width="400px",heigh="200px"))
                 )
   ),
   sidebarLayout(position="left",
-                sidebarPanel(h4("Prior information"),
-                             h4("- on the reproduction ratio"),
-                             selectInput("r0disttype","Distribution type",choices=c("","Non-informative","Log-normal","Gamma","Uniform")),
-                             conditionalPanel(condition="output.r0unif==true",
-                                              numericInput("r0min","Minimum",min=0,max=15,value=NA,step=.1),
-                                              numericInput("r0max","Maximum",min=0,max=15,value=NA,step=.1)),
+                sidebarPanel(h4("Prior distribution on the reproduction number"),
+                             selectInput("r0disttype","Distribution type",choices=c("-","Uniform","Exponential","Normal","Gamma")),
                              conditionalPanel(condition="output.r0meansd==true",
-                                              numericInput("r0mean","Mean",min=0,max=15,value=NA,step=.1),
-                                              numericInput("r0sd","Standard deviation",min=0,max=15,value=NA,step=.1)),
-                             br(),
-                             h4("- on the reporting rate"),
-                             selectInput("rhodisttype","Distribution type",choices=c("","Non-informative","Beta","Uniform")),
-                             numericInput("rhomean","Mean (in weeks)",min=0,max=1,value=NA,step=0.05),
-                             numericInput("rhosd","Standard deviation (in weeks)      ",min=0,max=1,value=NA,step=0.05)
+                                              numericInput("r0mean","Mean",min=0,max=15,value=NA,step=0.1),
+                                              numericInput("r0sd","Standard deviation",min=0,max=15,value=NA,step=0.1)),
+                             conditionalPanel(condition="output.r0unif==true",
+                                              numericInput("r0min","Minimum",min=0,max=15,value=NA,step=0.1),
+                                              numericInput("r0max","Maximum",min=0,max=15,value=NA,step=0.1)),
+                             conditionalPanel(condition="output.r0exp==true",
+                                              numericInput("r0rate","Rate",min=0,max=15,value=NA,step=0.1)),
+                             selectInput("exr0","or use a provided example",choices=c("-","Non-informative prior distribution","Chikungunya virus in Martinique, 2013-2015"))
                 ),
-                mainPanel(column(10,plotOutput(outputId="plotprior",width="400px",heigh="200px"))
+                mainPanel(column(10,plotOutput(outputId="plotpriorr0",width="400px",heigh="200px"))
                 )
-  )
+  ),
+  sidebarLayout(position="left",
+                sidebarPanel(h4("Prior distribution on the reporting rate"),
+                             selectInput("rhodisttype","Distribution type",choices=c("-","Uniform","Beta")),
+                             conditionalPanel(condition="output.rhounif==true",
+                                              numericInput("rhomin","Minimum",min=0,max=1,value=NA,step=0.05),
+                                              numericInput("rhomax","Maximum",min=0,max=1,value=NA,step=0.05)),
+                             conditionalPanel(condition="output.rhobetad==true",
+                                              numericInput("rhoshape1","Shape 1 (alpha)",min=0,max=10000,value=NA,step=1),
+                                              numericInput("rhoshape2","Shape 2 (beta)",min=0,max=10000,value=NA,step=1)),
+                             selectInput("exrho","or use a provided example",choices=c("-","Non-informative prior distribution","Chikungunya virus in Martinique, 2013-2015"))
+                ),
+                mainPanel(column(10,plotOutput(outputId="plotpriorrho",width="400px",heigh="200px"))
+                )
+  ),
+  
+  
+  
+  br(),br(),br()
 )
